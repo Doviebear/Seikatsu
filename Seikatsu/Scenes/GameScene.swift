0d13fc2fdac3e52494ff045f91f6b8484dc9b698
Dovie Shalev <Dovie.shalev@gmail.com>
2019-07-15 10:57:38 -0400

    Set Up

---
+++ Seikatsu/Scenes/GameScene.swift
@@ -0,0 +1,23 @@
+//
+//  GameScene.swift
+//  Seikatsu
+//
+//  Created by Dovie Shalev on 7/15/19.
+//  Copyright © 2019 Dovie Shalev. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+   
+    override func sceneDidLoad() {
+
+    }
+    
+    
+   
+    
+
+}
